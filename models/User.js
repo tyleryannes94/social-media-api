175f88f1b31e5b5ef83cca88d672b0490c71daf7
tyleryannes94 <tyler.yannes@gmail.com>
2024-01-18 21:20:02 -0600

    Completed the User.js mongodb model.

---
+++ models/User.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const userSchema = new Schema({
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+\@.+\..+/, 'Please fill a valid email address'] 
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought' 
+      }
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+      }
+    ]
+  }, {
+    toJSON: {
+      virtuals: true
+    },
+    id: false
+  });
+
+  userSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+  });
+  
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
