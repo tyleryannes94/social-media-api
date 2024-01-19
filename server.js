36876d22cc37d80b4de041de9560874e00e26292
tyleryannes94 <tyler.yannes@gmail.com>
2024-01-18 21:45:22 -0600

    started server.js code.

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const userRoutes = require('./routes/userRoutes');
+const thoughtRoutes = require('./routes/thoughtRoutes');
+
+const app = express();
+app.use(express.json());
