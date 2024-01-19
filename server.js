544d3faad746a82218605c3a7e7943d0bb880d15
tyleryannes94 <tyler.yannes@gmail.com>
2024-01-18 21:46:03 -0600

    added dotenv

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const userRoutes = require('./routes/userRoutes');
 const thoughtRoutes = require('./routes/thoughtRoutes');
+require('dotenv').config();
 
 const app = express();
 app.use(express.json());
