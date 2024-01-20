b2ef3032cffe048a5f45e3aa5a74627311695eb6
tyleryannes94 <tyler.yannes@gmail.com>
2024-01-19 19:10:23 -0600

    fixed userRoutes after receiving fails in insomnia.

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
-const Thought = require('../models/Thought'); // Assuming you have a Thought model
+const Thought = require('../models/Thought');
 
 // GET all users
 router.get('/', async (req, res) => {
@@ -14,24 +14,15 @@ router.get('/', async (req, res) => {
 });
 
 // GET a single user by its _id and populated thought and friend data
-router.get('/:id', async (req, res) => {
-    try {
-        const user = await User.findById(req.params.id).populate('thoughts').populate('friends');
-        if (!user) {
-            return res.status(404).json({ message: 'User not found' });
-        }
-        res.json(user);
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
+router.get('/:id', getUser, (req, res) => {
+    res.json(res.user);
 });
 
 // POST a new user
 router.post('/', async (req, res) => {
     const user = new User({
         username: req.body.username,
-        email: req.body.email,
-        // Include other fields as necessary
+        email: req.body.email
     });
     try {
         const newUser = await user.save();
@@ -42,27 +33,27 @@ router.post('/', async (req, res) => {
 });
 
 // PUT to update a user by its _id
-router.put('/:id', async (req, res) => {
+router.put('/:id', getUser, async (req, res) => {
+    if (req.body.username != null) {
+        res.user.username = req.body.username;
+    }
+    if (req.body.email != null) {
+        res.user.email = req.body.email;
+    }
+    // Add other fields as necessary
     try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
-        if (!user) {
-            return res.status(404).json({ message: 'User not found' });
-        }
-        res.json(user);
+        const updatedUser = await res.user.save();
+        res.json(updatedUser);
     } catch (err) {
         res.status(400).json({ message: err.message });
     }
 });
 
 // DELETE to remove user by its _id
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', getUser, async (req, res) => {
     try {
-        const user = await User.findById(req.params.id);
-        if (!user) {
-            return res.status(404).json({ message: 'User not found' });
-        }
-        await Thought.deleteMany({ username: user.username }); // Bonus: Remove user's associated thoughts
-        await user.remove();
+        await Thought.deleteMany({ username: res.user.username });
+        await res.user.remove();
         res.json({ message: 'User deleted' });
     } catch (err) {
         res.status(500).json({ message: err.message });
@@ -102,5 +93,20 @@ router.delete('/:userId/friends/:friendId', async (req, res) => {
         res.status(400).json({ message: err.message });
     }
 });
+
+// getUser middleware
+async function getUser(req, res, next) {
+    let user;
+    try {
+        user = await User.findById(req.params.id).populate('thoughts').populate('friends');
+        if (user == null) {
+            return res.status(404).json({ message: 'Cannot find user.' });
+        }
+    } catch (err) {
+        return res.status(500).json({ message: err.message });
+    }
+    res.user = user;
+    next();
+}
 
 module.exports = router;
